922bbf9dfd572238f899c6a9a2c8d164c8dfae54
Antonin Herrero <herrero.antonin@gmail.com>
2019-11-03 20:04:14 +0100

    (feat) Ajout de l'animation de grosse frappe

---
+++ Assets/Scripts/FPS/HugeFrappe.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HugeFrappe : MonoBehaviour
+{
+    private Animator animator;
+
+    public void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    public void Update()
+    {
+        if (Input.GetButton("Fire1")) {
+            animator.Play("huge frappe");
+        }
+    }
+}
