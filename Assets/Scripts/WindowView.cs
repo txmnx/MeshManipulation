c125b40a5db9ad570d5ac5bcbd56be1af69e4ac8
Antonin Herrero <herrero.antonin@gmail.com>
2019-11-03 20:08:15 +0100

    (feat) Suppression de la souris

---
+++ Assets/Scripts/WindowView.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Global operations on game window
+ */
+public class WindowView : MonoBehaviour
+{
+    private bool isCursorLocked = false;
+
+    void Update()
+    {
+        // DEBUG
+        if (Input.GetKey(KeyCode.Space)) {
+            if (isCursorLocked) {
+                Cursor.lockState = CursorLockMode.None;
+            }
+            else {
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+
+            isCursorLocked = !isCursorLocked;
+        }
+
+    }
+}
